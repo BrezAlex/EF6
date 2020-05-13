b78f166f1f4c4696ca3892dd9a78bbc5be6848ba
Alexandr Brezin <sasha.brezin7262@gmail.com>
2020-05-13 12:26:01 +0300

    Добавлены классы и база данных

---
+++ EF6/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+ 
+namespace EF6
+{
+    class UserContext : DbContext
+    {
+        public UserContext()
+            : base("DbConnection")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
